b67f6791ce4232e24f0717d847607feb0d09bf16
Shravya <shravya@workongrid.com>
2024-09-29 18:10:14 +0530

    added userModel for backend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,15 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const User = require("../models/userModel");
 
-router.post("/register", (req, res) => {
-    console.log("this is called")
-    res.status(200).json({message: "All is well"})
-})
+router.post("/register", async (req, res) => {
+  try {
+    const requestBody = req.body;
+    const newUser = new User(requestBody);
+    await newUser.save();
+    res.status(200).json({ success: true, message: "User has been created" });
+  } catch (e) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
 
-module.exports = router
+module.exports = router;
