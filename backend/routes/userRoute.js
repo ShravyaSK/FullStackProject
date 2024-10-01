c80963fad0edc4c229a722cc717ddf7d2fb22f22
Shravya <shravya@workongrid.com>
2024-10-01 14:50:50 +0530

    Recommit without sensitive information

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,34 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const User = require("../models/userModel");
 
-router.post("/register", (req, res) => {
-    console.log("this is called")
-    res.status(200).json({message: "All is well"})
+router.post("/register", async (req, res) => {
+  try {
+    const requestBody = req.body;
+    const newUser = new User(requestBody);
+    await newUser.save();
+    res.status(200).json({ success: true, message: "User has been created" });
+  } catch (e) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+    const user = await User.findOne({ email: req.body.email })
+    if(!user) {
+        return res.send({
+            success: false,
+            message: "User does not exist"
+        })
+    }
+    const isPassWordValid = user.password === req.body.password
+    if(!isPassWordValid) {
+        return res.send({
+            success: false,
+            message: "Password is invalid"
+        })
+    }
+    res.status(200).json({ success: true, message: "Logged in"})
 })
 
-module.exports = router
+
+module.exports = router;
